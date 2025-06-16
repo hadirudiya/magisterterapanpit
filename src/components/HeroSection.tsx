4117f5b45ed7001319f2177a1c1288226eb6106b
[dyad] <git@dyad.sh>
2025-06-17 03:09:25 +0700

    [dyad] Menghapus logo dari header - wrote 1 file(s)

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -59,8 +59,7 @@ const HeroSection = () => {
       {/* Top Navigation Bar */}
       <nav className="relative z-10 flex items-center justify-between p-4 bg-white text-gray-900 shadow-md">
         <div className="flex items-center space-x-2">
-          {/* Polimedia Logo */}
-          <img src="/logoPolimediaJPEG.jpg" alt="Polimedia Logo" className="h-10" />
+          {/* Polimedia Logo (removed) */}
         </div>
         {/* Mobile menu icon would go here */}
       </nav>
