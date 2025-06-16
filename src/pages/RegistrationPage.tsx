1f0f5657420655945b44817099dd13b5b8b8c57f
[dyad] <git@dyad.sh>
2025-06-17 02:40:04 +0700

    [dyad] Memperbarui tanggal matrikulasi - wrote 1 file(s)

--- src/pages/RegistrationPage.tsx
+++ src/pages/RegistrationPage.tsx
@@ -67,7 +67,7 @@ const RegistrationPage = () => {
                         )}
                         style={{ padding: '0.5rem 1rem' }} // Manual padding to match button size
                       >
-                        <svg xmlns="http://www.w3.org/2020/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-bar-chart-3 mr-2 h-4 w-4"><path d="M3 3v18h18"/><path d="M18 17V9"/><path d="M13 17V5"/><path d="M8 17v-3"/></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-bar-chart-3 mr-2 h-4 w-4"><path d="M3 3v18h18"/><path d="M18 17V9"/><path d="M13 17V5"/><path d="M8 17v-3"/></svg>
                         Konfirmasi Kehadiran Sosialisasi SMB Magister
                       </a>
                     </TableCell>
@@ -106,7 +106,7 @@ const RegistrationPage = () => {
                   </TableRow>
                   <TableRow>
                     <TableCell className="font-medium">Matrikulasi</TableCell>
-                    <TableCell colSpan={2} className="text-center">6 Agustus - 15 Agustus 2025</TableCell>
+                    <TableCell colSpan={2} className="text-center">15 September 2025 - 20 September 2025</TableCell>
                   </TableRow>
                   <TableRow className="bg-gray-100 dark:bg-gray-700">
                     <TableCell className="font-semibold text-gray-900 dark:text-white">Awal perkuliahan semester Ganjil 2025/2026</TableCell>
