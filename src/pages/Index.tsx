27a30cd4a31aa515d55986fe3427f40cbcde67eb
[dyad] <git@dyad.sh>
2025-06-16 16:13:38 +0700

    [dyad] wrote 1 file(s), deleted 1 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,11 +1,9 @@
-import { MadeWithDyad } from "@/components/made-with-dyad";
 import LandingPage from "./LandingPage"; // Import the new LandingPage component
 
 const Index = () => {
   return (
     <>
       <LandingPage /> {/* Render the LandingPage component */}
-      <MadeWithDyad />
     </>
   );
 };
